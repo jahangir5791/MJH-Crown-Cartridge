4f3fdd96c5861bbeb701105ddd3dcf1639446c94
jahangir5791 <jenisaniaini@gmail.com>
2026-05-18 14:44:48 +0300

    Add Launcher for game initialization
    Main launcher that loads game launcher scene after splash screen

--- 0_Launcher/Scripts/Launcher.cs
+++ 0_Launcher/Scripts/Launcher.cs
@@ -1,55 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class Launcher : MonoBehaviour
 {
-    [Header("UI Buttons")]
-    public Button survivorShooterButton;
-    public Button kingsReturnButton;
-    public Button quitButton;
-
-    [Header("Settings")]
-    public string survivorShooterScene = "Level_Shooter_01";
-    public string kingsReturnScene = "Level_King_Kingdom";
-
     void Start()
     {
-        if (survivorShooterButton != null)
-            survivorShooterButton.onClick.AddListener(PlaySurvivorShooter);
-
-        if (kingsReturnButton != null)
-            kingsReturnButton.onClick.AddListener(PlayKingsReturn);
-
-        if (quitButton != null)
-            quitButton.onClick.AddListener(QuitGame);
-
-        // অডিও ম্যানেজার চেক
-        if (AudioManager.Instance == null)
-        {
-            Debug.LogWarning("AudioManager not found in scene!");
-        }
+        Debug.Log("Launching on platform: " + PlatformDetector.GetPlatformName());
+        
+        // Auto-load game launcher after splash
+        Invoke("LoadGameLauncher", 2f);
     }
-
-    public void PlaySurvivorShooter()
+    
+    void LoadGameLauncher()
     {
-        AudioManager.Instance?.PlaySFX("Button_Click");
-        SceneLoader.Instance?.LoadScene(survivorShooterScene);
-    }
-
-    public void PlayKingsReturn()
-    {
-        AudioManager.Instance?.PlaySFX("Button_Click");
-        SceneLoader.Instance?.LoadScene(kingsReturnScene);
-    }
-
-    public void QuitGame()
-    {
-        AudioManager.Instance?.PlaySFX("Button_Click");
-        #if UNITY_EDITOR
-            UnityEditor.EditorApplication.isPlaying = false;
-        #else
-            Application.Quit();
-        #endif
+        SceneManager.LoadScene("GameLauncher");
     }
 }
