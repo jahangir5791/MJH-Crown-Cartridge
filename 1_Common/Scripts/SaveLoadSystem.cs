df5c16c43fa723ce2e31597467400aac1d092ead
jahangir5791 <jenisaniaini@gmail.com>
2026-05-18 17:08:24 +0300

    Add SaveLoadSystem for game persistence
    Save and load game data using JSON serialization

--- 1_Common/Scripts/SaveLoadSystem.cs
+++ 1_Common/Scripts/SaveLoadSystem.cs
@@ -1,97 +1,50 @@
 using UnityEngine;
 using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Collections.Generic;
 
 public class SaveLoadSystem : MonoBehaviour
 {
-    public static SaveLoadSystem Instance { get; private set; }
-
     private string savePath;
-
-    void Awake()
+    
+    void Start()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-            savePath = Application.persistentDataPath + "/savegame.dat";
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+        savePath = Application.persistentDataPath + "/savegame.dat";
     }
-
-    public void SaveGame(SaveData data)
+    
+    public void SaveGame(GameData data)
     {
-        try
-        {
-            BinaryFormatter formatter = new BinaryFormatter();
-            using (FileStream stream = new FileStream(savePath, FileMode.Create))
-            {
-                formatter.Serialize(stream, data);
-            }
-            Debug.Log($"Game saved to {savePath}");
-        }
-        catch (System.Exception e)
-        {
-            Debug.LogError($"Failed to save game: {e.Message}");
-        }
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(savePath, json);
+        Debug.Log("Game saved!");
     }
-
-    public SaveData LoadGame()
+    
+    public GameData LoadGame()
     {
         if (File.Exists(savePath))
         {
-            try
-            {
-                BinaryFormatter formatter = new BinaryFormatter();
-                using (FileStream stream = new FileStream(savePath, FileMode.Open))
-                {
-                    SaveData data = formatter.Deserialize(stream) as SaveData;
-                    Debug.Log("Game loaded successfully");
-                    return data;
-                }
-            }
-            catch (System.Exception e)
-            {
-                Debug.LogError($"Failed to load game: {e.Message}");
-            }
+            string json = File.ReadAllText(savePath);
+            GameData data = JsonUtility.FromJson<GameData>(json);
+            Debug.Log("Game loaded!");
+            return data;
         }
-        return null;
+        return new GameData();
     }
-
-    public bool HasSaveFile()
-    {
-        return File.Exists(savePath);
-    }
-
+    
     public void DeleteSave()
     {
         if (File.Exists(savePath))
         {
             File.Delete(savePath);
-            Debug.Log("Save file deleted");
+            Debug.Log("Save deleted!");
         }
     }
 }
 
 [System.Serializable]
-public class SaveData
+public class GameData
 {
-    public int playerScore;
     public int playerLevel;
-    public float playerHealth;
-    public Vector3 playerPosition;
-    public List<string> unlockedWeapons;
-    public Dictionary<string, int> resources;
-    public List<string> completedQuests;
-
-    public SaveData()
-    {
-        unlockedWeapons = new List<string>();
-        resources = new Dictionary<string, int>();
-        completedQuests = new List<string>();
-    }
+    public int score;
+    public int currency;
+    public bool isUnlocked;
+    public string lastLevel;
 }
