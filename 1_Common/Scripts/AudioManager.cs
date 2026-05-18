fd4ff8203043ca31f72b653b38d2fbfb14aa184f
jahangir5791 <jenisaniaini@gmail.com>
2026-05-18 15:00:09 +0300

    Add AudioManager for BGM and SFX
    Centralized audio management for background music and sound effects

--- 1_Common/Scripts/AudioManager.cs
+++ 1_Common/Scripts/AudioManager.cs
@@ -1,94 +1,55 @@
 using UnityEngine;
 using System.Collections.Generic;
-using System;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance { get; private set; }
-
-    [Header("Audio Sources")]
-    [SerializeField] private AudioSource bgmSource;
-    [SerializeField] private AudioSource sfxSource;
-
-    [Header("Audio Clips")]
-    [SerializeField] private List<AudioClip> bgmClips;
-    [SerializeField] private List<AudioClip> sfxClips;
-
-    private Dictionary<string, AudioClip> bgmDictionary = new Dictionary<string, AudioClip>();
-    private Dictionary<string, AudioClip> sfxDictionary = new Dictionary<string, AudioClip>();
-
-    [Header("Settings")]
-    [Range(0f, 1f)] public float bgmVolume = 0.5f;
-    [Range(0f, 1f)] public float sfxVolume = 0.7f;
-
+    
+    public List<AudioClip> bgmClips = new List<AudioClip>();
+    public List<AudioClip> sfxClips = new List<AudioClip>();
+    
+    private AudioSource bgmSource;
+    private AudioSource sfxSource;
+    
     void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
-            InitializeDictionaries();
+            
+            bgmSource = gameObject.AddComponent<AudioSource>();
+            sfxSource = gameObject.AddComponent<AudioSource>();
+            
+            bgmSource.loop = true;
         }
         else
         {
             Destroy(gameObject);
         }
     }
-
-    void InitializeDictionaries()
+    
+    public void PlayBGM(string clipName)
     {
-        foreach (var clip in bgmClips)
-        {
-            if (clip != null)
-                bgmDictionary[clip.name] = clip;
-        }
-        foreach (var clip in sfxClips)
-        {
-            if (clip != null)
-                sfxDictionary[clip.name] = clip;
-        }
-    }
-
-    public void PlayBGM(string clipName, bool loop = true)
-    {
-        if (bgmDictionary.TryGetValue(clipName, out AudioClip clip))
+        var clip = bgmClips.Find(c => c.name == clipName);
+        if (clip != null && !bgmSource.isPlaying)
         {
             bgmSource.clip = clip;
-            bgmSource.loop = loop;
-            bgmSource.volume = bgmVolume;
             bgmSource.Play();
         }
-        else
-        {
-            Debug.LogWarning($"BGM clip '{clipName}' not found!");
-        }
     }
-
+    
     public void PlaySFX(string clipName)
     {
-        if (sfxDictionary.TryGetValue(clipName, out AudioClip clip))
+        var clip = sfxClips.Find(c => c.name == clipName);
+        if (clip != null)
         {
-            sfxSource.PlayOneShot(clip, sfxVolume);
-        }
-        else
-        {
-            Debug.LogWarning($"SFX clip '{clipName}' not found!");
+            sfxSource.PlayOneShot(clip);
         }
     }
-
+    
     public void StopBGM()
     {
         bgmSource.Stop();
-    }
-
-    public void SetBGMVolume(float volume)
-    {
-        bgmVolume = Mathf.Clamp01(volume);
-        bgmSource.volume = bgmVolume;
-    }
-
-    public void SetSFXVolume(float volume)
-    {
-        sfxVolume = Mathf.Clamp01(volume);
     }
 }
