596af0150cc746d77c79c23b88f782d3dd667ba9
jahangir5791 <jenisaniaini@gmail.com>
2026-05-19 13:07:11 +0300

    Add shared type definitions
    JSDoc type definitions for shared data structures

---
+++ packages/shared/types/index.js
@@ -0,0 +1,25 @@
+// JSDoc types for shared types
+
+/**
+ * @typedef {Object} GameData
+ * @property {number} playerLevel
+ * @property {number} score
+ * @property {number} currency
+ * @property {boolean} isUnlocked
+ * @property {string} lastLevel
+ */
+
+/**
+ * @typedef {Object} User
+ * @property {string} id
+ * @property {string} email
+ * @property {string} username
+ * @property {'Android'|'iOS'|'Windows'|'macOS'|'Linux'|'WebGL'} platform
+ */
+
+/**
+ * @typedef {Object} Match
+ * @property {string} roomID
+ * @property {string} gameMode
+ * @property {number} skillLevel
+ */
